a43c3ab1047ce98bb9bb2562606cf4f5d23f0a52
Jared Stevens <JaredStevens@Jareds-Mac-mini.local>
2015-07-08 14:48:38 -0700

    Initial Commit

---
+++ TelePictionary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TelePictionary
+//
+//  Created by Jared Stevens on 7/8/15.
+//  Copyright (c) 2015 Claven Solutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
